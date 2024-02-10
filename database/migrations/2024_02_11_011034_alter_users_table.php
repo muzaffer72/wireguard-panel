290522ad7f60c27893359af9125bd0c0c09f9d76
David Suwandi <davidssuwandi@gmail.com>
2024-02-11 01:22:09 +0700

    patch connect api server

---
+++ database/migrations/2024_02_11_011034_alter_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->bigInteger('server_id')->unsigned()->after('upload')->nullable();
+            $table->foreign("server_id")->references("id")->on('servers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['server_id']);
+            $table->dropColumn(['server_id']);
+        });
+    }
+};
