d170a960efc2cc2bc700298292eec604026a3356
cakra budiman <cakrabudiman@icloud.com>
2024-03-26 16:17:51 +0700

    modified:   app/Http/Controllers/Api/SubscriptionController.php 	modified:   composer.json 	modified:   composer.lock

--- app/Http/Controllers/Api/SubscriptionController.php
+++ app/Http/Controllers/Api/SubscriptionController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Plan;
-use App\Models\Subscription;
+use App\Models\Subscription as Subs;
+use Imdhemy\GooglePlay\Subscriptions\SubscriptionPurchase;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Validator;
-use ReceiptValidator\iTunes\Validator as iTunesValidator;
-use Google_Client;
-use Google_Service_AndroidPublisher;
-use ReceiptValidator\GooglePlay\Validator as GooglePlayValidator;
+use Imdhemy\Purchases\Facades\Subscription;
+
+;
 
 class SubscriptionController extends Controller
 {
@@ -46,81 +46,77 @@ class SubscriptionController extends Controller
     private function validateAndUpdateITunesSubscription(Request $request)
     {
         $receiptBase64Data = $request->input('receipt_data');
-        $yourSharedSecret = $request->input('shared_secret');
 
-        $validator = new iTunesValidator(iTunesValidator::ENDPOINT_PRODUCTION);
 
         try {
-            // Validate receipt with shared secret for recurring subscriptions
-            $response = $validator->setSharedSecret($yourSharedSecret)
-                ->setReceiptData($receiptBase64Data)
-                ->validate();
+            $receiptResponse = Subscription::appStore()->receiptData($receiptBase64Data)->verifyRenewable();
+            $receiptStatus = $receiptResponse->getStatus();
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
 
         // Proceed with update only if receipt is valid
-        if ($response->isValid()) {
+        if ($receiptStatus->isValid()) {
             $user = auth('api')->user();
-            foreach ($response->getLatestReceiptInfo() as $purchase) {
-                $expiresDate = $purchase->getExpiresDate();
-                $subscription = Subscription::where('user_id', $user->id)->first();
+            $latestReceiptInfo = $receiptResponse->getLatestReceiptInfo();
+            $receiptInfo = $latestReceiptInfo[0];
 
-                if (!$subscription) {
-                    return response()->json(['error' => 'Subscription not found'], 404);
-                }
+            $expiresDate = $receiptInfo->getExpiresDate();
+            $subscription = Subs::where('user_id', $user->id)->first();
 
-                $validator = Validator::make($request->all(), [
-                    'plan' => ['required', 'integer'],
-                    'price' => ['required'],
-                    'payment_gateway_id' => ['required'],
-                    'type' => ['required'],
-                    'status' => ['required'],
-                ]);
+            if (!$subscription) {
+                return response()->json(['error' => 'Subscription not found'], 404);
+            }
 
-                if ($validator->fails()) {
-                    return response()->json(['errors' => $validator->errors()]);
-                }
+            $validator = Validator::make($request->all(), [
+                'plan' => ['required', 'integer'],
+                'price' => ['required'],
+                'payment_gateway_id' => ['required'],
+                'type' => ['required'],
+                'status' => ['required'],
+            ]);
 
-                $plan = Plan::findOrFail($request->input('plan'));
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()]);
+            }
 
-                $updateSubscription = $subscription->update([
-                    'plan_id' => $plan->id,
-                    'expiry_at' => $expiresDate,
-                    'status' => 1,
-                ]);
+            $plan = Plan::findOrFail($request->input('plan'));
 
-                if ($updateSubscription) {
-                    // Insert into transaction table
-                    $trx = new Transaction();
-                    $trx->checkout_id = date("YmdHis") . "-" . $user->id . "-" . $plan->id;
-                    $trx->user_id = $user->id;
-                    $trx->plan_id = $plan->id;
-                    $trx->price = $purchase->isTrialPeriod() ? "0" : $request->price;
-                    $trx->total = $request->price;
-                    $data = array(
-                        "price" => $request->price,
-                        "tax" => "0.00",  // Nilai pajak tetap 0.00 seperti yang diminta dalam JSON awal
-                        "total" => $request->price
-                    );
-                    $trx->details_before_discount = (object) $data;
-                    $trx->payment_gateway_id = $request->payment_gateway_id;
-                    $trx->payment_id = $purchase->getOriginalTransactionId();
-                    $trx->payer_email = $user->email;
-                    $trx->type = $request->type;
-                    $trx->status = $request->status;
-                    $trx->is_viewed = 0;
-                    $trx->save();
+            $updateSubscription = $subscription->update([
+                'plan_id' => $plan->id,
+                'expiry_at' => $expiresDate,
+                'status' => 1,
+            ]);
 
-                    $subs = $subscription->first();
-                    $receipt = $response->getReceipt();
-                    return response()->json(['receipt' => $receipt, 'message' => __('Successfully update subscription data')]);
-                }
+            if ($updateSubscription) {
+                // Insert into transaction table
+                $trx = new Transaction();
+                $trx->checkout_id = date("YmdHis") . "-" . $user->id . "-" . $plan->id;
+                $trx->user_id = $user->id;
+                $trx->plan_id = $plan->id;
+                $trx->price = $request->price;
+                $trx->total = $request->price;
+                $data = array(
+                    "price" => $request->price,
+                    "tax" => "0.00",  // Nilai pajak tetap 0.00 seperti yang diminta dalam JSON awal
+                    "total" => $request->price
+                );
+                $trx->details_before_discount = (object) $data;
+                $trx->payment_gateway_id = $request->payment_gateway_id;
+                $trx->payment_id = $receiptInfo->getOriginalTransactionId();
+                $trx->payer_email = $user->email;
+                $trx->type = $request->type;
+                $trx->status = $request->status;
+                $trx->is_viewed = 0;
+                $trx->save();
+
+                $receipt = $receiptResponse->getLatestReceiptInfo();
+                return response()->json(['receipt' => $receipt, 'message' => __('Successfully update subscription data')]);
             }
         } else {
             return response()->json([
                 'valid' => false,
-                'result_code' => $response->getResultCode()
+                'result_code' => $receiptResponse->getStatus()
             ]);
         }
     }
@@ -133,89 +129,68 @@ class SubscriptionController extends Controller
      */
     private function validateAndUpdateGooglePlaySubscription(Request $request)
     {
-        $packageName = $request->input('package_name');
         $productId = $request->input('product_id');
         $purchaseToken = $request->input('purchase_token');
-        $pathToServiceAccountJsonFile = 'YOUR_PATH_TO_JSON_FILE';
-        $googleClient = new Google_Client();
-        $googleClient->setScopes([Google_Service_AndroidPublisher::ANDROIDPUBLISHER]);
-        $googleClient->setApplicationName('Your_Purchase_Validator_Name');
-        $googleClient->setAuthConfig($pathToServiceAccountJsonFile);
-
-        $googleAndroidPublisher = new Google_Service_AndroidPublisher($googleClient);
-        $validator = new GooglePlayValidator($googleAndroidPublisher);
 
         try {
-            $response = $validator->setPackageName($packageName)
-                ->setProductId($productId)
-                ->setPurchaseToken($purchaseToken)
-                ->validateSubscription();
+            $subscriptionReceipt = Subscription::googlePlay()->id($productId)->token($purchaseToken)->get();
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
 
-        if ($response->isValid()) {
-            $user = auth('api')->user();
-            foreach ($response->validateSubscriptionV2() as $purchase) {
-                $expiresDate = $purchase->getPurchase();
-                $subscription = Subscription::where('user_id', $user->id)->first();
+        $user = auth('api')->user();
+        $expiresDate = $subscriptionReceipt->getExpiryTime();
+        $subscription = Subscription::where('user_id', $user->id)->first();
 
-                if (!$subscription) {
-                    return response()->json(['error' => 'Subscription not found'], 404);
-                }
-
-                $validator = Validator::make($request->all(), [
-                    'plan' => ['required', 'integer'],
-                    'price' => ['required'],
-                    'payment_gateway_id' => ['required'],
-                    'type' => ['required'],
-                    'status' => ['required'],
-                ]);
-
-                if ($validator->fails()) {
-                    return response()->json(['errors' => $validator->errors()]);
-                }
-
-                $plan = Plan::findOrFail($request->input('plan'));
-
-                $updateSubscription = $subscription->update([
-                    'plan_id' => $plan->id,
-                    'expiry_at' => $expiresDate,
-                    'status' => 1,
-                ]);
-
-                if ($updateSubscription) {
-                    // Insert into transaction table
-                    $trx = new Transaction();
-                    $trx->checkout_id = date("YmdHis") . "-" . $user->id . "-" . $plan->id;
-                    $trx->user_id = $user->id;
-                    $trx->plan_id = $plan->id;
-                    $trx->price = $purchase->getExpiresDate() ? "0" : $request->price;
-                    $trx->total = $request->price;
-                    $data = array(
-                        "price" => $request->price,
-                        "tax" => "0.00",  // Nilai pajak tetap 0.00 seperti yang diminta dalam JSON awal
-                        "total" => $request->price
-                    );
-                    $trx->details_before_discount = (object) $data;
-                    $trx->payment_gateway_id = $request->payment_gateway_id;
-                    $trx->payment_id = $purchase->getOriginalTransactionId();
-                    $trx->payer_email = $user->email;
-                    $trx->type = $request->type;
-                    $trx->status = $request->status;
-                    $trx->is_viewed = 0;
-                    $trx->save();
-
-                    $subs = $subscription->first();
-                    $receipt = $response->getReceipt();
-                    return response()->json(['receipt' => $receipt, 'message' => __('Successfully update subscription data')]);
-                }
-            }
-        } else {
-            return response()->json([
-                'valid' => false,
-                'result_code' => $response->getResultCode()
-            ]);
+        if (!$subscription) {
+            return response()->json(['error' => 'Subscription not found'], 404);
         }
+
+        $validator = Validator::make($request->all(), [
+            'plan' => ['required', 'integer'],
+            'price' => ['required'],
+            'payment_gateway_id' => ['required'],
+            'type' => ['required'],
+            'status' => ['required'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()]);
+        }
+
+        $plan = Plan::findOrFail($request->input('plan'));
+
+        $updateSubscription = $subscription->update([
+            'plan_id' => $plan->id,
+            'expiry_at' => $expiresDate,
+            'status' => 1,
+        ]);
+
+        if ($updateSubscription) {
+            // Insert into transaction table
+            $trx = new Transaction();
+            $trx->checkout_id = date("YmdHis") . "-" . $user->id . "-" . $plan->id;
+            $trx->user_id = $user->id;
+            $trx->plan_id = $plan->id;
+            $trx->price = $request->price;
+            $trx->total = $request->price;
+            $data = array(
+                "price" => $request->price,
+                "tax" => "0.00",  // Nilai pajak tetap 0.00 seperti yang diminta dalam JSON awal
+                "total" => $request->price
+            );
+            $trx->details_before_discount = (object) $data;
+            $trx->payment_gateway_id = $request->payment_gateway_id;
+            $trx->payment_id = $subscriptionReceipt->getOrderId();
+            $trx->payer_email = $user->email;
+            $trx->type = $request->type;
+            $trx->status = $request->status;
+            $trx->is_viewed = 0;
+            $trx->save();
+
+            $subs = $subscription->first();
+            return response()->json(['receipt' => $subs, 'message' => __('Successfully update subscription data')]);
+        }
+
     }
 }
