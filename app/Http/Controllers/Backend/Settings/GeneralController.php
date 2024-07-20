a99d9c5c8e82ebd20f6abca73e1b55c6d852162e
Caqil <cakrabudiman@icloud.com>
2024-07-20 10:48:25 +0000

    modified:   app/Http/Controllers/Api/AuthController.php 	modified:   app/Http/Controllers/Backend/Settings/GeneralController.php 	modified:   app/Models/Settings.php

--- app/Http/Controllers/Backend/Settings/GeneralController.php
+++ app/Http/Controllers/Backend/Settings/GeneralController.php
@@ -131,7 +131,6 @@ class GeneralController extends Controller
 
         setEnv('APP_URL', $requestData['general']['site_url']);
         setEnv('APP_TIMEZONE', "'{$requestData['general']['timezone']}'");
-        setEnv('MAX_ACTIVE_DEVICES', $requestData['general']['active_device_limit']);
         $colorsFile = 'assets/css/colors.css';
         if (!file_exists($colorsFile)) {
             fopen($colorsFile, "w");
