ed8dc4250a19dd664cc7d0391241fea0ea9e0232
cakra budiman <cakrabudiman@icloud.com>
2024-07-20 12:34:16 +0700

    modified:   database/seeders/DatabaseSeeder.php 	new file:   database/seeders/data/server.json 	modified:   public/images/hero-image.png 	modified:   resources/views/backend/includes/styles.blade.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Admin;
 use App\Models\Extension;
 use App\Models\Faq;
+use App\Models\Server;
 use App\Models\FooterMenu;
 use App\Models\Language;
 use App\Models\MailTemplate;
@@ -33,16 +34,16 @@ class DatabaseSeeder extends Seeder
         $json = json_decode(file_get_contents(database_path('seeders/data/admins.json')), true);
         foreach ($json as $row) {
             Admin::create([
-                'id'                => $row['id'],
-                'name'              => $row['name'],
-                'firstname'         => $row['firstname'],
-                'lastname'          => $row['lastname'],
-                'email'             => $row['email'],
-                'avatar'            => $row['avatar'],
-                'password'          => $row['password'],
-                'remember_token'    => $row['remember_token'],
-                'created_at'        => $row['created_at'],
-                'updated_at'        => $row['updated_at'],
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'firstname' => $row['firstname'],
+                'lastname' => $row['lastname'],
+                'email' => $row['email'],
+                'avatar' => $row['avatar'],
+                'password' => $row['password'],
+                'remember_token' => $row['remember_token'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
 
@@ -52,15 +53,15 @@ class DatabaseSeeder extends Seeder
         foreach ($json as $row) {
             $credentials = (object) json_decode($row['credentials']);
             Extension::create([
-                'id'                  => $row['id'],
-                'name'                => $row['name'],
-                'alias'               => $row['alias'],
-                'logo'                => $row['logo'],
-                'credentials'         => $credentials,
-                'instructions'        => $row['instructions'],
-                'status'              => $row['status'],
-                'created_at'          => $row['created_at'],
-                'updated_at'          => $row['updated_at'],
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'alias' => $row['alias'],
+                'logo' => $row['logo'],
+                'credentials' => $credentials,
+                'instructions' => $row['instructions'],
+                'status' => $row['status'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
 
@@ -69,12 +70,12 @@ class DatabaseSeeder extends Seeder
         $json = json_decode(file_get_contents(database_path('seeders/data/faqs.json')), true);
         foreach ($json as $row) {
             Faq::create([
-                'id'          => $row['id'],
-                'lang'        => $row['lang'],
-                'title'       => $row['title'],
-                'content'     => $row['content'],
-                'created_at'  => $row['created_at'],
-                'updated_at'  => $row['updated_at'],
+                'id' => $row['id'],
+                'lang' => $row['lang'],
+                'title' => $row['title'],
+                'content' => $row['content'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
 
@@ -83,14 +84,14 @@ class DatabaseSeeder extends Seeder
         $json = json_decode(file_get_contents(database_path('seeders/data/footer-menu.json')), true);
         foreach ($json as $row) {
             FooterMenu::create([
-                'id'          => $row['id'],
-                'name'        => $row['name'],
-                'link'        => $row['link'],
-                'lang'        => $row['lang'],
-                'parent_id'   => $row['parent_id'],
-                'order'       => $row['order'],
-                'created_at'  => $row['created_at'],
-                'updated_at'  => $row['updated_at'],
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'link' => $row['link'],
+                'lang' => $row['lang'],
+                'parent_id' => $row['parent_id'],
+                'order' => $row['order'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
 
@@ -99,14 +100,14 @@ class DatabaseSeeder extends Seeder
         $json = json_decode(file_get_contents(database_path('seeders/data/languages.json')), true);
         foreach ($json as $row) {
             Language::create([
-                'id'          => $row['id'],
-                'name'        => $row['name'],
-                'flag'        => $row['flag'],
-                'code'        => $row['code'],
-                'direction'   => $row['direction'],
-                'sort_id'     => $row['sort_id'],
-                'created_at'  => $row['created_at'],
-                'updated_at'  => $row['updated_at'],
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'flag' => $row['flag'],
+                'code' => $row['code'],
+                'direction' => $row['direction'],
+                'sort_id' => $row['sort_id'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
 
@@ -116,14 +117,14 @@ class DatabaseSeeder extends Seeder
         foreach ($json as $row) {
             $shortcodes = (object) json_decode($row['shortcodes']);
             MailTemplate::create([
-                'id'          => $row['id'],
-                'lang'        => $row['lang'],
-                'alias'       => $row['alias'],
-                'name'        => $row['name'],
-                'subject'     => $row['subject'],
-                'body'        => $row['body'],
-                'shortcodes'  => $shortcodes,
-                'status'      => $row['status'],
+                'id' => $row['id'],
+                'lang' => $row['lang'],
+                'alias' => $row['alias'],
+                'name' => $row['name'],
+                'subject' => $row['subject'],
+                'body' => $row['body'],
+                'shortcodes' => $shortcodes,
+                'status' => $row['status'],
             ]);
         }
 
@@ -132,14 +133,14 @@ class DatabaseSeeder extends Seeder
         $json = json_decode(file_get_contents(database_path('seeders/data/navbar-menu.json')), true);
         foreach ($json as $row) {
             NavbarMenu::create([
-                'id'          => $row['id'],
-                'name'        => $row['name'],
-                'link'        => $row['link'],
-                'lang'        => $row['lang'],
-                'parent_id'   => $row['parent_id'],
-                'order'       => $row['order'],
-                'created_at'  => $row['created_at'],
-                'updated_at'  => $row['updated_at'],
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'link' => $row['link'],
+                'lang' => $row['lang'],
+                'parent_id' => $row['parent_id'],
+                'order' => $row['order'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
 
@@ -150,20 +151,20 @@ class DatabaseSeeder extends Seeder
             $supported_currencies = json_decode($row['supported_currencies']);
             $credentials = (object) json_decode($row['credentials']);
             PaymentGateway::create([
-                'id'                   => $row['id'],
-                'name'                 => $row['name'],
-                'alias'                => $row['alias'],
-                'handler'              => $row['handler'],
-                'logo'                 => $row['logo'],
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'alias' => $row['alias'],
+                'handler' => $row['handler'],
+                'logo' => $row['logo'],
                 'supported_currencies' => $supported_currencies,
-                'fees'                 => $row['fees'],
-                'min'                  => $row['min'],
-                'test_mode'            => $row['test_mode'],
-                'credentials'          => $credentials,
-                'instructions'         => $row['instructions'],
-                'status'               => $row['status'],
-                'created_at'           => $row['created_at'],
-                'updated_at'           => $row['updated_at'],
+                'fees' => $row['fees'],
+                'min' => $row['min'],
+                'test_mode' => $row['test_mode'],
+                'credentials' => $credentials,
+                'instructions' => $row['instructions'],
+                'status' => $row['status'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
 
@@ -172,19 +173,37 @@ class DatabaseSeeder extends Seeder
         $json = json_decode(file_get_contents(database_path('seeders/data/plans.json')), true);
         foreach ($json as $row) {
             Plan::create([
-                'id'                   => $row['id'],
-                'name'                 => $row['name'],
-                'product_id'    => $row['product_id'],
-                'interval'             => $row['interval'],
-                'price'                => $row['price'],
-                'expiration'           => $row['expiration'],
-                'advertisements'       => $row['advertisements'],
-                'custom_features'      => $row['custom_features'],
-                'is_free'              => $row['is_free'],
-                'login_require'        => $row['login_require'],
-                'is_featured'          => $row['is_featured'],
-                'created_at'           => $row['created_at'],
-                'updated_at'           => $row['updated_at'],
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'product_id' => $row['product_id'],
+                'interval' => $row['interval'],
+                'price' => $row['price'],
+                'expiration' => $row['expiration'],
+                'advertisements' => $row['advertisements'],
+                'custom_features' => $row['custom_features'],
+                'is_free' => $row['is_free'],
+                'login_require' => $row['login_require'],
+                'is_featured' => $row['is_featured'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
+            ]);
+        }
+
+        // Server table
+        Server::truncate();
+        $json = json_decode(file_get_contents(database_path('seeders/data/server.json')), true);
+        foreach ($json as $row) {
+            Server::create([
+                'country' => $row['country'],
+                'state' => $row['state'],
+                'latitude' => $row['v'],
+                'longitude' => $row['longitude'],
+                'status' => $row['status'],
+                'ip_address' => $row['ip_address'],
+                'recommended' => $row['recommended'],
+                'is_premium' => $row['is_premium'],
+                'is_ovpn' => $row['is_ovpn'],
+                'ovpn_config' => $row['ovpn_config'],
             ]);
         }
 
@@ -193,15 +212,15 @@ class DatabaseSeeder extends Seeder
         $json = json_decode(file_get_contents(database_path('seeders/data/seo-configurations.json')), true);
         foreach ($json as $row) {
             SeoConfiguration::create([
-                'id'                  => $row['id'],
-                'lang'                => $row['lang'],
-                'title'               => $row['title'],
-                'description'         => $row['description'],
-                'keywords'            => $row['keywords'],
-                'robots_index'        => $row['robots_index'],
+                'id' => $row['id'],
+                'lang' => $row['lang'],
+                'title' => $row['title'],
+                'description' => $row['description'],
+                'keywords' => $row['keywords'],
+                'robots_index' => $row['robots_index'],
                 'robots_follow_links' => $row['robots_follow_links'],
-                'created_at'          => $row['created_at'],
-                'updated_at'          => $row['updated_at'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
 
@@ -211,9 +230,9 @@ class DatabaseSeeder extends Seeder
         foreach ($json as $row) {
             $value = (object) $row['value'];
             Settings::create([
-                'id'                 => $row['id'],
-                'key'                => $row['key'],
-                'value'              => $value
+                'id' => $row['id'],
+                'key' => $row['key'],
+                'value' => $value
             ]);
         }
 
@@ -222,18 +241,18 @@ class DatabaseSeeder extends Seeder
         $json = json_decode(file_get_contents(database_path('seeders/data/countries.json')), true);
         foreach ($json as $row) {
             Country::create([
-                'id'              => $row['id'],
-                'code'            => $row['code'],
-                'name'            => $row['name'],
-                'capital'         => $row['capital'],
-                'continent'       => $row['continent'],
-                'continent_code'  => $row['continent_code'],
-                'phone'           => $row['phone'],
-                'currency'        => $row['currency'],
-                'symbol'          => $row['symbol'],
-                'alpha_3'         => $row['alpha_3'],
-                'created_at'      => $row['created_at'],
-                'updated_at'      => $row['updated_at'],
+                'id' => $row['id'],
+                'code' => $row['code'],
+                'name' => $row['name'],
+                'capital' => $row['capital'],
+                'continent' => $row['continent'],
+                'continent_code' => $row['continent_code'],
+                'phone' => $row['phone'],
+                'currency' => $row['currency'],
+                'symbol' => $row['symbol'],
+                'alpha_3' => $row['alpha_3'],
+                'created_at' => $row['created_at'],
+                'updated_at' => $row['updated_at'],
             ]);
         }
     }
--- resources/views/backend/includes/styles.blade.php
+++ resources/views/backend/includes/styles.blade.php
@@ -4,7 +4,8 @@
 
 <!-- Core CSS -->
 <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/core.css') }}" class="template-customizer-core-css">
-<link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css') }}" class="template-customizer-theme-css">
+<link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css') }}"
+    class="template-customizer-theme-css">
 <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}">
 
 <!-- Vendors CSS -->
